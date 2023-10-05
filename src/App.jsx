5fc6df39f3a13a1bc108a8fe500cabbb33ae4f67
Het Gajera <het36100@gmail.com>
2023-10-05 14:55:51 +0530

    image & effect update

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,47 @@
 import { useState } from "react";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import "./App.css";
+import TextBlock from "./TextBlock";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Parallax pages={1} style={{ top: "0", left: "0" }} className="animation">
-        <ParallaxLayer offset={0} speed={2.5}>
-          <div className="animation_layer parallax" id="back"></div>
+      <Parallax pages={2} style={{ top: "0", left: "0" }} className="animation">
+        <ParallaxLayer offset={0} speed={0}>
+          <div className="animation_layer parallax" id="background"></div>
         </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={2.5}>
-          <div className="animation_layer parallax" id="mid"></div>
+        <ParallaxLayer offset={0} speed={0.6}>
+          <div className="animation_layer parallax" id="mountain"></div>
         </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={2.5}>
-          <div className="animation_layer parallax" id="front"></div>
+        <ParallaxLayer offset={0} speed={0.65}>
+          <div className="animation_layer parallax" id="clouds"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.6}>
+          <div className="animation_layer parallax" id="sun"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.6}>
+          <div className="animation_layer parallax" id="title">
+            <h1 className="name">
+              Plant<span className="highlight">DEX</span>
+            </h1>
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.5}>
+          <div className="animation_layer parallax" id="backfield"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.55}>
+          <div className="animation_layer parallax" id="farmer"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.4}>
+          <div className="animation_layer parallax" id="frontfield"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.4}>
+          <div className="animation_layer parallax" id="corn"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0}>
+          <TextBlock />
         </ParallaxLayer>
       </Parallax>
     </>
---
+++ src/TextBlock.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function TextBlock() {
+  return (
+    <div className="textblock">
+      <div className="textblock-container">
+        <h1 className="textblock-title">The Farmer</h1>
+        <p className="textblock-content"></p>
+      </div>
+    </div>
+  );
+}
+
+export default TextBlock;
